fec46deb449efc48260715d60931131db7187863
KatsuXD55 <untukkontakku@gmail.com>
2023-02-07 19:55:37 +0700

    im-kAtsuu

---
+++ settings.js
@@ -0,0 +1,74 @@
+//Ubah Image/Thumbnail di folder maslent/pplent/maslent.png (Cari fotomu Ubah namnya ganti jadi maslent.png trus di gantu make pp mu yang baru
+const fs = require('fs')
+const chalk = require('chalk')
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+//==============================\
+
+global.pulsa = "628xxx"
+global.gopay = "62858781782928"
+global.dana = "62858781782928"
+global.ovo = "62858781782928"
+global.ownerlen = "Katsuu/Botz"//Ubah
+global.owner = ['62858781782928'] //ubh nomor owner hp
+global.ownername = "KatsuXD Official" //ubah
+global.ytname = "YT: KatsuXD Official"//ubah
+global.socialm = "IG: Katsubotz Official"//ubah
+global.location = "Indonesia, Yogyakarta, Kulon Progo"//ubah
+global.ownernomer = "62858781782928"//ubah nomorhp owner
+global.premium = ['62858781782928']//ubah nomor hp premium
+global.botname = 'BotWhatsappV1' //ubah
+global.linkz = "https://chat.whatsapp.com/JJ5jttEsYpAF2zvxkwAanN /LINKGC WA"//ubah
+global.websitex = "https://github.com/KatsuXD55"//ubah
+global.botscript = '-'
+global.themeemoji = "😿"//ubah
+global.packname = "Sticker By KatsuXD"//ubah
+global.author = "Made by Katsu77"//ubah
+global.wm = "Katsuu"//ubah
+global.sessionName = 'session'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+global.mess = {
+    success: 'Piye?!', //ubah aja make bhs indo terserahhh karepmu
+    admin: 'This feature could be used by admins only!',
+    botAdmin: 'Bot Must Be Admin First!',
+    premime: 'Premium Special Features If You Want to Register Type Rent',
+    owner: 'This feature could be used by owner only',
+    group: 'Features Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This feature could be used by bot only',
+    wait: 'In process...',
+    linkm: 'Where is the link?',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+}
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.thum = fs.readFileSync("./Maslent/pplent/maslent.jpg")
+global.log0 = fs.readFileSync("./Maslent/pplent/maslent.jpg")
+global.err4r = fs.readFileSync("./Maslent/pplent/maslent.jpg")
+global.thumb = fs.readFileSync("./Maslent/pplent/maslent.jpg")
+    global.limitawal = {
+    premium: "Infinity",
+    free: 12, // limit awall 
+    monayawal: 1000
+}
+global.limitAwal = {
+ prem: 'Unlimited',
+ free: 70
+}
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
